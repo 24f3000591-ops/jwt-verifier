784ef8ab5e34009758d02136cc7200f52bb006d8
24f3000591-ops <24f3000591@ds.study.iitm.ac.in>
2026-07-04 09:18:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import jwt
 
 app = FastAPI()
 
-PUBLIC_KEY = """
------BEGIN PUBLIC KEY-----
+PUBLIC_KEY = """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
 cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
 EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
 WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
 ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
-SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
-dQIDAQAB
------END PUBLIC KEY-----
-"""
+SI6iyrYbKR0NEBSqq4XkadEjsCs4LlgniT7GlkL9Mce3b0wGLs9/7ZIXdQIDAQAB
+-----END PUBLIC KEY-----"""
 
 ISSUER = "https://idp.exam.local"
 AUDIENCE = "tds-qia71wrf.apps.exam.local"
@@ -24,11 +22,16 @@ class TokenRequest(BaseModel):
     token: str
 
 
+@app.get("/")
+def home():
+    return {"status": "running"}
+
+
 @app.post("/verify")
-def verify(req: TokenRequest):
+def verify(request: TokenRequest):
     try:
         payload = jwt.decode(
-            req.token,
+            request.token,
             PUBLIC_KEY,
             algorithms=["RS256"],
             issuer=ISSUER,
@@ -42,8 +45,8 @@ def verify(req: TokenRequest):
             "aud": payload.get("aud"),
         }
 
-    except jwt.PyJWTError:
-        raise HTTPException(
+    except Exception:
+        return JSONResponse(
             status_code=401,
-            detail={"valid": False},
+            content={"valid": False},
         )
